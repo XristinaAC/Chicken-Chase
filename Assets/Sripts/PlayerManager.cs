ca70ac02a50bbdbd4d3cbfbc13e45f65d3b9a5f1
Christina Papadodimitraki <christina-v7@hotmail.com>
2025-10-29 20:11:30 +0100

    Jumping-Glide

--- Assets/Sripts/PlayerManager.cs
+++ Assets/Sripts/PlayerManager.cs
@@ -9,10 +9,12 @@ public class Player : MonoBehaviour
     [SerializeField] private float playerSpeed = 0;
     [SerializeField] private float jumpingSpeed = 50;
     [SerializeField] private float gravity = 0;
+    [SerializeField] private LayerMask mask;
 
     private Vector3 _runningVelocity = Vector3.zero;
     private Vector3 _jumpingVelocity = Vector3.zero;
     private bool _obstacleHit = false;
+    private bool _isHoldingSpace = false;
 
     private bool _isJumping = false;
 
@@ -27,7 +29,6 @@ public class Player : MonoBehaviour
         if (!_obstacleHit)
         {
             transform.position += _runningVelocity * Time.deltaTime;
-            //DetectCollision();
         }
         
         if(_isJumping == false)
@@ -35,37 +36,41 @@ public class Player : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 _isJumping = true;
+                _isHoldingSpace = true;
                 _jumpingVelocity.y = jumpingSpeed;
             }
         }
-    }
 
-    //private void Gliding()
-    //{ 
-    //    if(Input.GetKeyDown(KeyCode.Space))
-    //    {
-    //        //Gliding code here
-    //    }
-    //}
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            _isHoldingSpace = false;
+        }
+    }
 
     private void FixedUpdate()
     {
-        Vector3 pos = transform.position;
         if(_isJumping)
         {
-            this.GetComponent<Rigidbody>().AddForce(_jumpingVelocity.x, _jumpingVelocity.y, 0);
-            _jumpingVelocity.y += gravity * Time.fixedDeltaTime;
-            //if(_obstacleHit) { _obstacleHit = false; }
-                
+            this.GetComponent<Rigidbody>().AddForce(0, _jumpingVelocity.y, 0);
+
+            if (_isHoldingSpace == false)
+            {
+                _jumpingVelocity.y += gravity * Time.fixedDeltaTime;
+            }
+            else
+            {
+                this.GetComponent<Rigidbody>().AddForce(20, 0, 0);
+            }
             DetectCollision();
         }
     }
 
     private void DetectCollision()
     {
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.up), out hit, 5.0f))
+        RaycastHit hi;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.up),out hi, 10.0f))
         {
+            Debug.Log("hi");
             _isJumping = false;
         }
     }
@@ -73,6 +78,10 @@ public class Player : MonoBehaviour
     //Turn that into raycasts
     private void OnCollisionEnter(Collision collision)
     {
+        if (collision.gameObject.tag == "Ground")
+        {
+            _isJumping = false;
+        }
         if (collision.gameObject.tag == "obstacle")
         {
             this.gameObject.SetActive(false);
