73a15d24043b54cc14b651e1b50d0dd518a1c9f1
GargeyaD <mrmarvelous2002@gmail.com>
2025-11-05 18:25:14 +0100

    shelves

--- Assets/Programmer/Christina/Sripts/Managers/PlayerManager2.cs
+++ Assets/Programmer/Christina/Sripts/Managers/PlayerManager2.cs
@@ -38,7 +38,7 @@ public class PlayerManager2 : MonoBehaviour
 
     private void Start()
     {
-        _runningVelocity = new Vector3(playerSpeed * 0.009f, 0, 0);
+       
         _rbDrag = this.GetComponent<Rigidbody>().drag;
         _jumpHeightV = new Vector3(0, _jumpHeight, 0);
         SetDirection(0);
@@ -72,7 +72,7 @@ public class PlayerManager2 : MonoBehaviour
     {
         if (!_obstacleHit)
         {
-            transform.position += new Vector3(playerSpeed * 0.009f, 0, 0); ;
+            transform.position += new Vector3(playerSpeed * Time.deltaTime, 0, 0); ;
         }
     }
 
