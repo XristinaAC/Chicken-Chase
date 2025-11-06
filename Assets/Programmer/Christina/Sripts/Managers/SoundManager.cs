41630a50c53a6ede501fe11e6add64881ad87c86
Christina Papadodimitraki <christina-v7@hotmail.com>
2025-11-07 00:26:48 +0100

    SoundManager1

--- Assets/Programmer/Christina/Sripts/Managers/SoundManager.cs
+++ Assets/Programmer/Christina/Sripts/Managers/SoundManager.cs
@@ -4,8 +4,52 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    private List<AudioClip> _backgroundMusic = null;
-    private List<AudioClip> _soundEffects = null;
+    public static SoundManager Instance = null;
+
+    public enum AudioTypes
+    {
+        backgroundMusic,
+        death,
+        buttonAudioEffects,
+        jumpingAudioEffects,
+        glidingAudioEffects,
+    }
+
+    [Header("_____________AudioSource_______________")]
+
+    [SerializeField] private AudioSource MusicSource;
+    [SerializeField] private AudioSource SFXSource;
 
 
+    [Header("_____________AudioClips_______________")]
+
+    [SerializeField] public AudioClip backgroundMusic;
+    [SerializeField] public AudioClip buttonEffect;
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+    
+    public void PlayMusic(AudioClip music)
+    {
+        MusicSource.clip = music;
+        MusicSource.Play();
+    }
+
+    public void PlaySFX(AudioClip sfxEffect)
+    {
+        SFXSource.clip = sfxEffect;
+        SFXSource.Play();
+    }
 }
