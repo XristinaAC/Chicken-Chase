7814051faf4a10fb887524e995b049591c9c9cd1
Christina Papadodimitraki <christina-v7@hotmail.com>
2025-11-06 14:06:44 +0100

    changes

--- Assets/Programmer/Christina/Sripts/MakeTurnManager/ChangeSceneScriptableObjects.cs
+++ Assets/Programmer/Christina/Sripts/MakeTurnManager/ChangeSceneScriptableObjects.cs
@@ -33,7 +33,7 @@ public class ChangeSceneScriptableObjects : ScriptableObject
             Collider[] col = Physics.OverlapSphere(cso[i].changeSceneArea.transform.position, 0.3f, player);
             if (col.Length > 0)
             {
-                col[0].GetComponent<PlayerManager>().SetDirection((int)cso[i].playerDirection);
+                col[0].GetComponent<PlayerManager2>().SetDirection((int)cso[i].playerDirection);
             }
         }
         
