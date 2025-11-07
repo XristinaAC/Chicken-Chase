241d0b4308e6b48d714ee5c044c2f237e59d6d9e
Christina Papadodimitraki <christina-v7@hotmail.com>
2025-11-08 00:48:54 +0100

    SnappierCameraControler

--- Assets/Programmer/Christina/Sripts/Managers/CameraManager.cs
+++ Assets/Programmer/Christina/Sripts/Managers/CameraManager.cs
@@ -32,7 +32,7 @@ public class CameraManager : MonoBehaviour
         else if(player.transform.position.y < transform.position.y - 5)
         {
             Vector3 newPos = new Vector3(0, player.transform.position.y + offset.y, 0);
-            transform.position = Vector3.Lerp(transform.position, newPos, 0.5f * Time.deltaTime);
+            transform.position = Vector3.Lerp(transform.position, newPos, 10 * Time.deltaTime);
             height = 0;
         }
         transform.position = new Vector3(player.transform.position.x + offset.x + 2, transform.position.y, player.transform.position.z + offset.z);
