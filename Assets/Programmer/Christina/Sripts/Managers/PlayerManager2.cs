d09cedb8410e7ac022d6976d7040721c3083359b
Christina Papadodimitraki <christina-v7@hotmail.com>
2025-11-08 20:26:11 +0100

    PlayerCotroler

--- Assets/Programmer/Christina/Sripts/Managers/PlayerManager2.cs
+++ Assets/Programmer/Christina/Sripts/Managers/PlayerManager2.cs
@@ -60,8 +60,6 @@ public class PlayerManager2 : MonoBehaviour
         }
     }
 
-    float height;
-    float counter = 0;
     private void Update()
     {
         if (GameManager.Instance.CurrentState != GameManager.GameState.Playing) return;
@@ -85,7 +83,6 @@ public class PlayerManager2 : MonoBehaviour
         {
             _isHoldingSpace = true;
             _isJumping = true;
-            counter = Time.time;
         }
     }
 
